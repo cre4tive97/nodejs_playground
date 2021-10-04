c9c23c3d996823a9f12f7223c851ab21831e4f53
cre4tive97 <harryborrison5148@gmail.com>
2021-10-05 04:07:49 +0900

    db set

--- server.js
+++ server.js
@@ -11,12 +11,30 @@ MongoClient.connect(
     }
     db = client.db("todoapp");
 
-    db.collection("post").insertOne(
-      { 이름: "John", 나이: 20 },
-      (error, result) => {
-        console.log("저장완료");
-      }
-    );
+    // db.collection("post").insertOne(
+    //   { _id: 3, 이름: "John", 나이: 20 },
+    //   (error, result) => {
+    //     console.log("저장완료");
+    //   }
+    // );
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+    app.get("/write", (req, res) => {
+      res.sendFile(__dirname + "/write.html");
+    });
+
+    app.post("/add", (req, res) => {
+      res.send("전송완료");
+      console.log(req.body.date);
+      console.log(req.body.title);
+      db.collection("post").insertOne(
+        { title: req.body.title, date: req.body.date },
+        (error, result) => {
+          console.log("저장완료");
+        }
+      );
+    });
 
     app.listen(8080, () => {
       console.log("listening on 8080");
